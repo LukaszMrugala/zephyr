658d1b5e8a85b8995d24a616f50be98511afc7fe
Graydon, Connor Jameson <connor.graydon@intel.com>
2020-06-25 17:53:18 -0700

    Try moving the set -/+ e around and see if that skirts it
    Signed-off-by: Graydon, Connor Jameson <connor.graydon@intel.com>

--- modules/merge.sh
+++ modules/merge.sh
@@ -230,7 +230,7 @@ echo
 echo "Back from sanitycheck-runner."
 
 # Add a pause here to allow things to finish writing out and settle before trying to run the parser. 
-sleep 20
+sleep 10
 
 echo "Calling $SCRIPT_PATH/get_failed.py"
 python3 $SCRIPT_PATH/get_failed.py $SANITY_OUT 
@@ -257,13 +257,13 @@ if [ "$GATE" == "true" ]; then
     echo "You have selected to gate the push and merge, so we are done now. Follow up manually."
     exit 
 elif [ "$GATE" == "false" ]; then
-    echo "We are not gated, so pushing the merge and tagging. (Not really, gated for testing.)"
+    echo "We are not gated, so pushing the merge and tagging."
     if ! git push origin HEAD:$MERGE_TO; then
         echo "Merge/tag push failed for some reason. Manual intervention needed."
        exit 1
     fi
 
-    echo "Tagging Branch: $MERGE_TO. (Also not really.)"
+    echo "Tagging Branch: $MERGE_TO."
  
     if ! make_tag; then
         echo "Something failed when tagging. Manual intervention required. Quitting!"
@@ -272,5 +272,3 @@ elif [ "$GATE" == "false" ]; then
 
     git push origin $TAG
 fi
-
-echo "DONE!"
