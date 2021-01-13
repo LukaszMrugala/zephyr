35b02fe1e572e4f999a02fb6560fe579c2abb265
Vondrachek, Chris <chris.vondrachek@intel.com>
2021-01-13 12:53:39 -0800

    auto-detect scripts/twister & use it instead of scripts/sanitycheck

--- modules/sanitycheck-runner.sh
+++ modules/sanitycheck-runner.sh
@@ -82,16 +82,21 @@ echo DOCKER_RUN=$DOCKER_RUN
 # Sanitycheck configuration & command-line generation
 export TESTCASES="testcases"
 
+# handle switch from sanitycheck -> twister, gracefully
+if [ -f "scripts/twister" ]; then
+    export SC_CMD_BASE="$DOCKER_RUN scripts/twister -x=USE_CCACHE=0 -N --inline-logs"
+else
+    export SC_CMD_BASE="$DOCKER_RUN scripts/sanitycheck -x=USE_CCACHE=0 -N --inline-logs"
+fi
+
+export SC_CMD_SAVE_TESTS="$SC_CMD_BASE -B $2/$1 $3 --save-tests $TESTCASES"
+
 #handle branch differences in twister / sanitycheck params + junit output
 if [ "$ZEPHYR_BRANCH_BASE" == "v1.14-branch-intel" ]; then
-    export SC_CMD_BASE="$DOCKER_RUN scripts/sanitycheck -x=USE_CCACHE=0 -N --inline-logs"
-    export SC_CMD_SAVE_TESTS="$SC_CMD_BASE -B $2/$1 $3 --save-tests $TESTCASES"
     export SC_CMD1="$SC_CMD_BASE -B $2/$1 -v --detailed-report $ZEPHYR_BASE/sanity-out/node$2-junit1.xml --load-tests $TESTCASES"
     export SC_CMD2="$SC_CMD_BASE -f -v --detailed-report $ZEPHYR_BASE/sanity-out/node$2-junit2.xml"
     export SC_CMD3="$SC_CMD_BASE -f -v --detailed-report $ZEPHYR_BASE/sanity-out/node$2-junit3.xml"
 elif [ "$ZEPHYR_BRANCH_BASE" == "master" ]; then
-    export SC_CMD_BASE="$DOCKER_RUN scripts/twister -x=USE_CCACHE=0 -N --inline-logs"
-    export SC_CMD_SAVE_TESTS="$SC_CMD_BASE -B $2/$1 $3 --save-tests $TESTCASES"
     export SC_CMD1="$SC_CMD_BASE --integration -v --load-tests $TESTCASES --retry-failed 4 --retry-interval 60"
 fi
 
