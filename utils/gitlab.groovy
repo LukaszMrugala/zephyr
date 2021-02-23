5390ce43682ac62c3913d7a7c380427c3d4d8184
chris.vondrachek <cvondra@zephyr-ci.jf.intel.com>
2021-02-23 12:00:19 -0800

    adding groovy modules for gitlab, jenkins-vis & west

---
+++ utils/gitlab.groovy
@@ -0,0 +1,50 @@
+// gitlab.groovy
+// zephyr devops jenkins-gitlab interface
+
+////////////////////////////////////////////////////////////////////////////////
+// clone - performs in-place clone of srcRepo/srcBranch with timeoutMins (opt)
+//	srcRepo - ssh or http url supported
+//	srcBranch - branch name, tag or sha
+//
+def clone(srcRepo,srcBranch,timeoutMins) {
+
+	//default 5 min timemout
+	timeoutMins = timeoutMins ?: 5
+
+	stage('gitlab-clone') {
+		echo "stage: git-checkout, params:"
+		echo "   srcRepo=${srcRepo}"
+		echo "   srcBranch=${srcBranch}"
+		//wrap git operation in a 5-minute timeout
+		timeout(time: timeoutMins, unit: 'MINUTES') {
+			checkout changelog: true, poll: false, scm: [
+				$class: 'GitSCM',
+				branches: [[name: "${srcBranch}"]],
+				userRemoteConfigs: [[url: "${srcRepo}"]]
+			]
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// setStatus - set commit status on gitlab
+//	jobStatus -  [pending, running, canceled, success, failed] ONLY
+//	jobName - must be set as public
+//
+def setStatus(jobStatus) {
+
+	updateGitlabCommitStatus name: "$jobName", state: "$jobStatus"
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// addMRComment - adds comment to merge request associated with gitlab commit
+//	commentStr -  comment string
+//	jobName - must be set as public
+//
+def addMRComment(commentStr) {
+	stage("add MR comment") {
+		addGitLabMRComment comment: "${commentStr}"
+	}
+}
+
+return this
---
+++ utils/west.groovy
@@ -0,0 +1,27 @@
+// west.groovy
+// zephyr devops west interface
+
+////////////////////////////////////////////////////////////////////////////////
+// run - performs west init-update with timeoutMins (opt)
+//	requires 'buildConfig' set from previous call to detect-branch
+//
+def run(timeoutMins) {
+	//default 15 min timemout
+	timeoutMins = timeoutMins ?: 15
+
+	stage('west')
+	{
+		dir('zephyrproject') {
+			//wrap in a retry + timeout block since these are external repos & may timeout
+			retry(3) {
+				timeout(time: timeoutMins, unit: 'MINUTES') {
+					withEnv(["ZEPHYR_BRANCH_BASE=${buildConfig['branchBase']}"]) {
+						sh "../ci/utils/west-init-update.sh"
+					}
+				}
+			}
+		}
+	}
+}
+
+return this
