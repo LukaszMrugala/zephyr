389c350c0b256e303131452cad4c5cc1fdb712b5
Lukasz Mrugala <lukaszx.mrugala@intel.com>
2024-01-25 07:57:10 +0000

    scripts: tests: Blackbox test expansion --quarantine-verify
    *     --quarantine-verify
    
    Signed-off-by: Lukasz Mrugala <lukaszx.mrugala@intel.com>

--- scripts/tests/twister_blackbox/test_lm.py
+++ scripts/tests/twister_blackbox/test_lm.py
@@ -589,3 +589,32 @@ class TestReport:
         res = re.search(pattern_dummy, inline_twister_log)
         assert res
 
+    @pytest.mark.usefixtures("clear_log")
+    @mock.patch.object(TestPlan, 'TESTSUITE_FILENAME', testsuite_filename_mock)
+    def test_quarantine_verify(self, out_path):
+        test_platforms = ['qemu_x86', 'frdm_k64f']
+        path = os.path.join(TEST_DATA, 'tests', 'dummy')
+        quarantine_path = os.path.join(TEST_DATA, 'twister-quarantine-list.yml')
+        args = ['-i', '--outdir', out_path, '-T', path, '-y'] + \
+               ['--quarantine-verify'] + \
+               ['--quarantine-list', quarantine_path] + \
+               [val for pair in zip(
+                   ['-p'] * len(test_platforms), test_platforms
+               ) for val in pair]
+
+        with mock.patch.object(sys, 'argv', [sys.argv[0]] + args), \
+                pytest.raises(SystemExit) as sys_exit:
+            self.loader.exec_module(self.twister_module)
+
+        with open(os.path.join(out_path, 'testplan.json')) as f:
+           j = json.load(f)
+        filtered_j = [
+           (ts['platform'], ts['name'], tc['identifier']) \
+               for ts in j['testsuites'] \
+               for tc in ts['testcases'] if 'reason' not in tc
+        ]
+
+        assert str(sys_exit.value) == '0'
+
+        assert len(filtered_j) == 2
+
